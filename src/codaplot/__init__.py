accc5261fe30fe2ebe1689432b7b14628ef62341
Stephen Kraemer <stephenkraemer@gmail.com>
2018-10-15 11:04:15 +0200

    update __init__.py

--- src/codaplot/__init__.py
+++ src/codaplot/__init__.py
@@ -1,4 +1,7 @@
 from .clustered_data_grid import ClusteredDataGrid
 from .plotting import categorical_heatmap, find_stretches
-from codaplot.clustered_data_grid import Heatmap
+from .clustered_data_grid import (
+    Heatmap, ClusterSizePlot, ColAggPlot, RowGroupAggPlot,
+    Violin, MultiLine, Dendrogram, AggHeatmap)
 from .dynamic_grid import GridManager, GridElement
+from .linkage_mat import Linkage
