1955ff184eef6244ba8eeca3c799dd15f4dbae31
Stephen Kraemer <stephenkraemer@gmail.com>
2018-10-30 11:20:13 +0100

    add missing dependencies to setup.py

--- setup.py
+++ setup.py
@@ -26,12 +26,24 @@ setup(
         data_files = [],
 
         install_requires=[
-            'pandas',
+            'matplotlib>=3.0.0',
+            'pandas>=0.23',
+            'seaborn>=0.9.0',
+            # currently needs private fixed version, still need to submit PR
+            'dynamicTreeCut',
+            # only for python 3.6, improve this
+            'dataclasses',
             'numpy',
-            'pytest',
-            'pytest-mock',
-            'matplotlib',
+            'numba',
+            'scipy',
+            'toolz',
+            'more_itertools',
         ],
         python_requires='>=3.6',
+        extras_require={
+            'dev': [
+                'pytest',
+            ]
+        }
 
 )
