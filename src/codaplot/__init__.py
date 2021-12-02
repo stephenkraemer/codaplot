4e80aa66733f9905dedd1e6dbbcff4ce03ddee85
Stephen Kraemer <stephenkraemer@gmail.com>
2021-12-02 17:57:47 +0100

    add more functions to co namespace

--- src/codaplot/__init__.py
+++ src/codaplot/__init__.py
@@ -1,5 +1,5 @@
 import codaplot.plotting
-from .array_manager import cross_plot, anno_axes, spacer, array_to_figure, add_guides
+from .array_manager import cross_plot, anno_axes, spacer, array_to_figure, add_guides, compute_gridspec_ratios
 from .clustered_data_grid import ClusteredDataGrid
 from .plotting import categorical_heatmap, find_stretches, spaced_heatmap, heatmap_depr, heatmap, frame_groups, label_groups, add_cluster_anno_bubbles, adjust_coords, cbar_change_style_to_inward_white_ticks
 from .flow_plot import flow_plot
