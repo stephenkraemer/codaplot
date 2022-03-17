f71ff5834b2f2be4c281d9f6dd9cc2ddb5ee2eb1
Stephen Kraemer <stephenkraemer@gmail.com>
2022-03-17 14:20:02 +0100

    improve jointplot
    - optional: display p-value
    - different kwds dicts for the two 1d hists
      eg to allow different colors

--- src/codaplot/jointplot.py
+++ src/codaplot/jointplot.py
@@ -1,21 +1,24 @@
 # # Imports
 
 import os
+from typing import Any, Dict, Literal, Optional, Tuple, Union
+
+import matplotlib as mpl
+import matplotlib.colors as mcolors
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import matplotlib.transforms as mtransforms
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-import matplotlib.ticker as mticker
-import matplotlib.colors as mcolors
-import matplotlib.transforms as mtransforms
-import matplotlib as mpl
+import scipy.stats
+import seaborn as sns
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from matplotlib.offsetbox import AnchoredOffsetbox, TextArea, VPacker
+
 import codaplot as co
 import codaplot.utils as coutils
-import seaborn as sns
-from typing import Union, Literal, Tuple, Dict, Optional, Any
-from matplotlib.figure import Figure
-from matplotlib.axes import Axes
-
 
 # # jointplot
 
@@ -27,12 +30,14 @@ def jointplot(
     ylabel,
     freq_label: str,
     hist2d_kwargs: Optional[Dict[str, Any]] = None,
-    hist1d_kwargs: Optional[Dict[str, Any]] = None,
+    x_hist1d_kwargs: Optional[Dict[str, Any]] = None,
+    y_hist1d_kwargs: Optional[Dict[str, Any]] = None,
     xlim=None,
     ylim=None,
     pcc_pos: Optional[
         Union[Literal["title", "upper center"], Tuple[float, float, str, str]]
     ] = "title",
+    show_pvalue=False,
     dist_axes_height_and_width_in=6 / 2.54,
     hist_height=1 / 2.54,
     cbar_height=0.3 / 2.54,
@@ -54,7 +59,7 @@ def jointplot(
        if not None, place as title of 2D axes ('title'), aligned to the top of the 2D axes ('upper center'), or at (x, y, ha, va) position
     hist2d_kwargs
        not: rasterized
-    hist1d_kwargs
+    x/y_hist1d_kwargs
        not: rasterized, orientiation
 
     Returns
@@ -64,12 +69,15 @@ def jointplot(
 
     # %%
     pd.testing.assert_index_equal(xscore.index, yscore.index)
-    if hist1d_kwargs is None:
-        hist1d_kwargs = {}
+    if x_hist1d_kwargs is None:
+        x_hist1d_kwargs = {}
+    if y_hist1d_kwargs is None:
+        y_hist1d_kwargs = {}
     if hist2d_kwargs is None:
         hist2d_kwargs = {}
     assert not {"rasterized"}.intersection(set(hist2d_kwargs.values()))
-    assert not {"rasterized", "orientation"}.intersection(set(hist1d_kwargs.values()))
+    assert not {"rasterized", "orientation"}.intersection(set(x_hist1d_kwargs.values()))
+    assert not {"rasterized", "orientation"}.intersection(set(y_hist1d_kwargs.values()))
 
     """
     Note that if the fig height /  fig width ratio is not chosen appropriately (such that the space for the 2d axes is square), there will be a lot of whitespace introduced by CL. It could be that the figure can also be arranged with little whitespace if these ratios are off, but that this is just not handled optimally by CL atm, see https://github.com/matplotlib/matplotlib/pull/17246 and https://github.com/mwaskom/seaborn/issues/2051. I am not sure if thats the case
@@ -127,17 +135,47 @@ def jointplot(
     axd["cbar"].set_xlabel(freq_label)
     co.cbar_change_style_to_inward_white_ticks(cb, remove_ticks_at_ends="always")
 
-    xhist_ax.hist(xscore, rasterized=True, **hist1d_kwargs)
+    xhist_ax.hist(xscore, rasterized=True, **x_hist1d_kwargs)
     xhist_ax.xaxis.set_visible(False)
     xhist_ax.set(ylabel=freq_label)
 
-    yhist_ax.hist(yscore, orientation="horizontal", rasterized=True, **hist1d_kwargs)
+    yhist_ax.hist(yscore, orientation="horizontal", rasterized=True, **y_hist1d_kwargs)
     yhist_ax.yaxis.set_visible(False)
     yhist_ax.set(xlabel=freq_label)
 
-    pcc = xscore.corr(yscore)
+    pd.testing.assert_index_equal(xscore.index, yscore.index)
+    pcc, two_tailed_pvalue = scipy.stats.pearsonr(
+        x=xscore.to_numpy(), y=yscore.to_numpy()
+    )
+
+
     pcc_str = f"PCC = {pcc:.2f}"
-    if pcc_pos == "title":
+    if show_pvalue and pcc_pos == "title":
+
+        pcc_str = TextArea(
+            f"PCC = {pcc:.2f}", textprops=dict(size=mpl.rcParams["axes.titlesize"])
+        )
+        log10_pvalue = np.log10(two_tailed_pvalue)
+        if log10_pvalue == -np.inf:
+            pval_text_size = max(5, mpl.rcParams["axes.titlesize"] - 2)
+            pval_str = TextArea("log10(p) < -100", textprops=dict(size=pval_text_size))
+        else:
+            pval_str = TextArea(
+                f"\nlog10(p) = {log10_pvalue.round(0):.0f}",
+                textprops=dict(size=mpl.rcParams["axes.titlesize"] - 1),
+            )
+        hpack = VPacker(children=[pcc_str, pval_str], align="center", pad=0, sep=2)
+        anchored_box = AnchoredOffsetbox(
+            loc="lower center",
+            child=hpack,
+            pad=0.0,
+            frameon=False,
+            bbox_to_anchor=(0.5, 1.05),
+            bbox_transform=hist2d_ax.transAxes,
+            borderpad=0.0,
+        )
+        hist2d_ax.add_artist(anchored_box)
+    elif pcc_pos == "title":
         hist2d_ax.set_title(pcc_str)
     elif pcc_pos == "upper center":
         xstart, xend = hist2d_ax.get_xlim()
@@ -156,8 +194,6 @@ def jointplot(
             x=pcc_pos[0],
             y=pcc_pos[1],
             s=pcc_str,
-            ha=pcc[pcc_pos[2]],
-            va=pcc[pcc_pos[3]],
             fontsize=mpl.rcParams["axes.labelsize"],
         )
 
