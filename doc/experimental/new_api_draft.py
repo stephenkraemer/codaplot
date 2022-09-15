e37124b9d62370f4eb8af366166de2495f104ffd
Stephen Kraemer <stephenkraemer@gmail.com>
2022-09-15 09:10:29 +0200

    api draft: Heatmap

--- doc/experimental/new_api_draft.py
+++ doc/experimental/new_api_draft.py
@@ -8,78 +8,167 @@ import codaplot as co
 
 
 # %% [markdown]
-# ## Simple heatmap
+# ## Simple heatmaps
+
+
+# %% [markdown]
+# ### Simple heatmap
 
 # %%
 fig, ax = plt.subplots(1, 1)
-h = (
-    co.Heatmap(
-        color=mlog10_pvalues,
+h = co.Heatmap(
+    color=mlog10_pvalues,
+    vmin=0,
+    vmax=3,
+    cmap="Reds",
+    color_legend_title="-log10(q-values)",
+    color_legend_cbar_kwds=dict(
+        shrink=0.5,
+        aspect=2,
+        expand="both",
+    ),
+).draw(ax=ax)
+ax.set(xlabel="abc", ylabel="abc")
+
+
+# %% [markdown]
+# ### Simple spaced heatmap
+
+# %%
+fig, ax = plt.subplots(1, 1)
+h = co.Heatmap(
+    color=mlog10_pvalues,
+    vmin=0,
+    vmax=3,
+    cmap="Reds",
+    color_legend_title="-log10(q-values)",
+    color_legend_cbar_kwds=dict(
+        shrink=0.5,
+        aspect=2,
+        expand="both",
+    ),
+    row_spacing_group_ids=list('aaaabbbbcccc'),
+    col_spacing_group_ids=np.tile([1, 2, 3], 5),
+).draw(ax=ax)
+ax.set(xlabel="abc", ylabel="abc")
+
+
+# %% [markdown]
+# ### Categorical heatmap
+
+# %%
+fig, ax = plt.subplots(1, 1)
+h = co.Heatmap(
+    color=categorical_data,
+    cmap="Set1",
+    color_legend_title="groups",
+    color_legend_legend_kwds=dict(loc=(0, 1), bbox_to_anchor='upper_left'),
+).draw(ax=ax)
+ax.set(xlabel="abc", ylabel="abc")
+
+
+
+# %% [markdown]
+# ## Powerful meshes
+
+# %% [markdown]
+# ### Meshes with multiple mark properties
+
+# %% [markdown]
+# - size is specified proportinal to rectangle space, so will always work with any figure size
+
+# %%
+for DemoMesh in [co.CircleMesh, co.RectangleMesh, co.TriangleMesh]:
+    fig, ax = plt.subplots(1, 1)
+    h = co.Heatmap(
+        color=log_odds,
+        size=p_values,
+        marker=DemoMesh(
             vmin=0,
             vmax=3,
+            cmap='Reds',
+            sizemin=0.1,
+            sizemax=0.9,
+            ),
+        color_legend_title="-log10(q-values)",
+        color_legend_cbar_kwds=dict(
+            shrink=0.5,
+            aspect=2,
+            expand="both",
+        ),
+        row_spacing_group_ids=list('aaaabbbbcccc'),
+        col_spacing_group_ids=np.tile([1, 2, 3], 5),
+    ).draw(ax=ax)
+    ax.set(xlabel="abc", ylabel="abc")
+
+# %% [markdown]
+# ### MultiMarkMesh
+
+
+# %% [markdown]
+# ## Handle multiple legends
+
+
+# %% [markdown]
+# ## Control mark placement to overlay complex information in one heatmap
+
+
+# %% [markdown]
+# ### Example 1: Methylation and expression side by side
+
+# %%
+fig, ax = plt.subplots(1, 1)
+
+shared_kwds = dict(
+    color_legend_cbar_kwds=dict(
+        shrink=0.4,
+        aspect=10,
+    ),
+    row_spacing_group_ids=list('aaaabbbbcccc'),
+    col_spacing_group_ids=np.tile([1, 2, 3], 5),
+    )
+
+# Add average methylation data
+meth_legend_d = co.Heatmap(
+    color=average_meth,
+    marker=co.TriangleMesh(
+        vmin=0,
+        vmax=1,
+        cmap='cividis',
+        ),
+    marker_placement_max_size=0.9,
+    marker_placement_loc=(0, 0),
+    marker_placement_bbox_to_anchor='upper_left',
+    color_legend_title="Average meth.",
+    **shared_kwds,
+).draw(ax=ax)
+
+
+# add expression data
+expr_legend_d = co.Heatmap(
+    color=expression_data,
+    marker=co.TriangleMesh(
+        vmin=0,
+        vmax=10,
         cmap='Reds',
-        color_legend_title="-log10(q-values)"
-        color_legend_cbar_kwds = dict(
-                shrink=0.5,
-                aspect=2,
-                expand="both",
-            ),
-        )
-    .draw(ax=ax)
-)
-ax.set(xlabel="abc", ylabel="abc")
+        ),
+    marker_placement_max_size=0.9,
+    marker_placement_loc=(1, 1),
+    marker_placement_bbox_to_anchor='lower_right',
+    color_legend_title="Normalized expression",
+    **shared_kwds
+).draw(ax=ax)
+
+co.add_legends([meth_legend_d, expr_legend_d], loc=(1.05, 0), bbox_to_anchor='upper left')
+
+ax.set(xlabel="Samples", ylabel="Genes")
+
+# Implementation notes
+# - both layers set the same ticklabels, so thats fine
 
 
 # %% [markdown]
-# ## Simple spaced heatmap
-
-# %%
-fig, ax = plt.subplots(1, 1)
-h = (
-    co.Heatmap()
-    .add_layer(
-        data=pvalues,
-        mesh=co.HeatmapMesh(
-            vmin="p1",
-            vmax="p99",
-            vcenter=0,
-            color_bar=ColorBarLegend(
-                title="-log10(q-values)",
-                shrink=0.5,
-                aspect=2,
-                expand="both",
-            ),
-        ),
-    )
-    .insert_spacers(
-        row_group_ids=dmr_cluster_compartments,
-        col_group_ids=pop_compartments,
-        row_spacer_sizes=0.01,
-        col_spacer_sizes=0.01,
-    )
-    .draw(ax=ax)
-)
-ax.set(xlabel="abc", ylabel="abc")
-
-
-# %% [markdown]
-# ## Categorical heatmap
-
-# %%
-fig, ax = plt.subplots(1, 1)
-h = (
-    co.Heatmap()
-    .add_layer(
-        data=categorical_df,
-        mesh=co.HeatmapMesh(
-            # only necessary when categorical df is not object, pd.Str or pd.Categorical
-            is_categorical=True,
-            cmap="Set1",
-            legend=co.CategoricalColorLegend(title="classes"),
-        ),
-    )
-    .draw_on_axes(ax)
-)
+# ### Example 2: Oncoprints
 
 
 # %% [markdown]
